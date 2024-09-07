86374a5a01847eb349951c9bd9854f08814857cc
abhi <ugabhi@proton.me>
2024-09-07 12:14:22 +0530

    -

--- mods/mod-org-tex.el
+++ mods/mod-org-tex.el
@@ -1,30 +1,12 @@
-; -------------------------------
+
 ; -- org publish
-; -------------------------------
+
 (use-package auctex
   :ensure t
   )
 (require 'tex)
 
 (push (list 'output-pdf "Zathura") TeX-view-program-selection)
-;(push (list 'output-pdf "Zathura") Tex-view-program-selection)
-
-(setq org-publish-use-timestamps-flag nil)
-(setq org-export-with-broken-links t)
-(setq org-publish-project-alist
-      '(("website"
-         :base-directory "~/web/website/"
-         :base-extension "org"
-         :publishing-directory "~/web/website/"
-         :recursive t
-         :exclude "org-html-themes/.*"
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4             ; Just the default for this project.
-         :auto-preamble t)))
-
-
-
-
 
 (setq org-publish-project-alist
       '(
@@ -36,8 +18,6 @@
          :recursive t                            ; Include subdirectories
          :publishing-function org-latex-publish-to-pdf ; Export to LaTeX (.tex)
          )))                     ; Automatically include preamble
-
-
 
 (require 'ox-latex)  ; Ensure org-latex export is loaded
 
@@ -54,4 +34,3 @@
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-(provide 'mod-org-tex)
---
+++ org-clock-save.el
@@ -0,0 +1,2 @@
+;; org-clock-save.el - nixos at <2024-09-07 Sat 12:12>
+(setq org-clock-stored-history '(("/home/abhi/documents/org/income-tax/salaries.org" . 286) ("/home/abhi/documents/org/income-tax/salaries.org" . 121)))
